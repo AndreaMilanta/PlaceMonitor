314b96f640a278d40783ebbbaa26e357c4dc2f82
AndreaMilanta <andrea.milanta@gmail.com>
2017-08-16 18:47:19 -0400

    login javascript working

--- server/javascript/login.js
+++ server/javascript/login.js
@@ -1,4 +1,17 @@
 function validateCreds(){
-    document.getElementbyId("errorPar").innerHTML = "function called";
+    var warningPar = document.getElementById("errorPar");
+    var mail = document.getElementById("email");
+    var password = document.getElementById("pswd");
+    if (checkUser(mail.value, password.value) === false) {
+        warningPar.innerHTML = "Incorrect username or password!";
+        warningPar.style.color = '#FF0000';
+        email.value = "";
+        password.value = "";
+        return false;
+    }
+    return true;
+}
+
+function checkUser(mail, password) {
     return false;
 }
